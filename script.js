197c73a3e78172edb819c06382f972005ca70bf5
Rohan Puri <127716446+rohanpuri81@users.noreply.github.com>
2023-05-05 17:24:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function change(){
+    document.getElementById("a1").src="tick-blue.png";
+} 
+function ret(){
+    document.getElementById("a1").src="tick-gray.png";
+}
+function change2(){
+    document.getElementById("a2").src="tick-blue.png";
+} 
+function ret2(){
+    document.getElementById("a2").src="tick-gray.png";
+}
+function change3(){
+    document.getElementById("a3").src="tick-blue.png";
+} 
+function ret3(){
+    document.getElementById("a3").src="tick-gray.png";
+}
+function change4(){
+    document.getElementById("a4").src="tick-blue.png";
+} 
+function ret4(){
+    document.getElementById("a4").src="tick-gray.png";
+}
